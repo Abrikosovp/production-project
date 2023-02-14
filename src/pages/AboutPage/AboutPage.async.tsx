293ba57aa96cbea75fb70767065e6478bc247754
Abrikosov Pavel <abrikosovp@mail.ru>
2023-02-15 01:05:49 +0300

    initial commit

---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,8 @@
+import {lazy} from "react";
+
+
+export const AboutPageAsync = lazy(() => new Promise((resolver) => {
+
+    // @ts-ignore
+    setTimeout(() => resolver(import('./AboutPage')), 2000)
+}));
