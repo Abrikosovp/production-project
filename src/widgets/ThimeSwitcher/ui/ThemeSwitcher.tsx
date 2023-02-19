57c88dd6f5ee768e498811b85c7d62ed0d2126d3
Abrikosov Pavel <abrikosovp@mail.ru>
2023-02-19 17:47:23 +0300

    add stylelint, i18n plugin for eslint, fix add styles

--- src/widgets/ThimeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThimeSwitcher/ui/ThemeSwitcher.tsx
@@ -5,7 +5,6 @@ import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
-import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -17,7 +16,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     return (
         <Button
             theme={ThemeButton.CLEAR}
-            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
             {
